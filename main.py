83df922aecb141ed067db54543e60d7a9bed803f
kwiacek175 <259378@student.pwr.edu.pl>
2024-01-08 21:31:18 +0100

    Dodanie pliku pyramid.py - rysowanie piramidki.

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from objects.pyramid import Pyramid
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+
+    quadric = gluNewQuadric()
+    gluQuadricNormals(quadric, GLU_SMOOTH)
+    gluQuadricTexture(quadric, GL_TRUE)
+
+    pyramid = Pyramid((0, 1, 0), 1.0)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        pyramid.draw(quadric)
+
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+if __name__ == "__main__":
+    main()
---
+++ objects/pyramid.py
@@ -0,0 +1,51 @@
+from OpenGL.GL import *
+from math import sqrt
+
+from OpenGL.raw.GLU import gluCylinder, gluDisk
+
+class Pyramid:
+    edges = (
+        (0, 1),
+        (1, 2),
+        (2, 0),
+
+        (3, 0),
+        (3, 1),
+        (3, 2)
+    )
+
+    surfaces = (
+        (0, 1, 2),
+        (2, 0, 3),
+        (1, 2, 3),
+        (0, 1, 3),
+    )
+
+    colors = (
+        (1, 0, 0),
+        (1, 1, 0),
+        (1, 0, 1),
+        (0, 0, 1),
+    )
+
+    def __init__(self, top, size):
+        self.height = size * sqrt(6) / 3
+        self.radius = size * sqrt(6) / 3
+        self.center = (top[0], top[1] - self.height, top[2])
+
+    def draw(self, quadric):
+        glTranslatef(self.center[0], self.center[1], self.center[2])
+        glRotatef(-90, 1, 0, 0)
+
+        gluCylinder(quadric, self.radius, 0, self.height, 3, 1)
+        gluDisk(quadric, 0, self.radius, 3, 1)
+
+        glRotatef(90, 1, 0, 0)
+        glTranslatef(-self.center[0], -self.center[1], -self.center[2])
+
+
+
+
+
+
+
